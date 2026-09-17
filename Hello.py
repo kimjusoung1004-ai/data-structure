3496f3b7eb2608748111d20a061852e17fe56d2f
kmj <kimjusoung104@gmail.com>
2026-09-17 12:50:07 +0900

    Hello

--- Hello.py
+++ Hello.py
@@ -1,1 +1,10 @@
-print(" Hello, word!")
+kakao = ["가나","다라", "마바", "사아", "자차"]
+print(kakao)
+kakao.append(None)
+print(kakao)
+kakao[4] = kakao[5]
+kakao[4] = None
+print(kakao)
+kakao[3] = kakao[4]
+kakao[3] = None
+print(kakao)
